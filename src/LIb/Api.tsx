f3426b2bd9cb65eb8be7a640d1cf20d943d0e5d8
miraz932 <programmar.miraz@gmail.com>
2026-09-26 22:24:43 +0600

    fetch

--- src/LIb/Api.tsx
+++ src/LIb/Api.tsx
@@ -1,7 +1,7 @@
 import { IDataType } from "@/Type";
 
 const getApidata =async ():Promise<IDataType[]> => {
-    const res = await fetch('https://api.abcz.workers.dev/api/fitlog',{cache:"force-cache"})
+    const res = await fetch('https://api.api-store.workers.dev/api/fitlog',{cache:"force-cache"})
     if(!res.ok){
         throw new Error('Fetch Failed Data')
     }
